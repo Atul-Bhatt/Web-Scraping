12b031f1859f2ae33d75954f419e45f6a02b12a2
Atul-Bhatt <atulbhatt61@gmail.com>
2021-03-27 20:41:49 +0530

    Documentation changed to md.

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -3,10 +3,12 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Product(Item):
+    name = Field()
+    price = Field()
+    stock = Field()
+    tags = Field()
+    last_updated = Field(serializer=str)
